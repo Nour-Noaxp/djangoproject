b56c6348e37c2f05e54c94662427f84a262390c9
nour <nour.ayed@noaxp.com>
2025-01-30 11:09:26 +0100

    Created Django model form that saves data to the db

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, HttpResponse
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+
+def new(request):
+  form = PostForm()
+  if request.method == 'POST':
+    print(request.POST)
+    form = PostForm(request.POST)
+    if form.is_valid():
+      form.save()
+
+  context = {'form':form}
+  return render(request, 'blog/new.html', context)
 
 def home(request):
   categories = ['culture', 'sports', 'food', 'politics', 'religion', 'nature']
   return render(request, 'blog/home.html', {'categories': categories})
 
 def post_list(request):
-  posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  posts = Post.objects.all
   return render(request, 'blog/post_list.html', {'posts': posts})
 
 def about(request):
