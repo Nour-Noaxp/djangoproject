0d3ca2c78c3a3addb57e8619ffef9099f7aa8690
Blatalzar <bataprod@gmail.com>
2025-01-31 15:11:55 +0100

    Code review

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,9 @@
 from django.forms import ModelForm, TextInput
 from .models import Post
 
+
 class PostForm(ModelForm):
-  class Meta:
-    model = Post
-    fields = '__all__'
-    widgets = {
-      'text': TextInput(attrs={'class': 'h-32'})
-    }
-    
+    class Meta:
+        model = Post
+        fields = "__all__"
+        widgets = {"text": TextInput(attrs={"class": "h-32"})}
--- blog/views.py
+++ blog/views.py
@@ -2,32 +2,37 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
+from django.utils import timezone
+
 
 def new(request):
-  form = PostForm()
-  return render(request, 'blog/new.html', {'form':form})
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Post successfully created")
+            return redirect("post_list")
+    return render(request, "blog/new.html", {"form": form})
 
-def create(request):
-  form = PostForm()
-  if request.method == 'POST':
-    print(request.POST)
-    form = PostForm(request.POST)
-    if form.is_valid():
-      form.save()
-      messages.success(request, "Post successfully created")
-      return redirect('post_list')
-  return render(request, 'blog/new.html', {'form':form})
 
 def home(request):
-  categories = ['culture', 'sports', 'food', 'politics', 'religion', 'nature']
-  return render(request, 'blog/home.html', {'categories': categories})
+    categories = ["culture", "sports", "food", "politics", "religion", "nature"]
+    return render(request, "blog/home.html", {"categories": categories})
+
 
 def post_list(request):
-  posts = Post.objects.all
-  return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return render(request, "blog/post_list.html", {"posts": posts})
+
 
 def about(request):
-  return render(request, 'blog/about.html')
+    return render(request, "blog/about.html")
+
 
 def contact(request):
-  return HttpResponse("This is my contact page displayed with an Http response and not a template rendering")
+    return HttpResponse(
+        "This is my contact page displayed with an Http response and not a template rendering"
+    )
