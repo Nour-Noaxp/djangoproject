78bb9acb91fd8a7aadbe023d3271643cb1c962e8
Nour <nour.ayed@noaxp.com>
2025-02-10 14:07:39 +0100

    Cleaned up code

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,3 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ["author", "title", "text", "published_date"]
-        # widgets = {"text": TextInput(attrs={"class": "h-32"})}
