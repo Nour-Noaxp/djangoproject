f1d40dd33c4b3244c20f98f58a70fafee34bfa15
Nour <nour.ayed@noaxp.com>
2025-02-11 18:43:29 +0100

    Added video templates,views and urls, integrated video objects display in the post_list template

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path("show/<post_id>", views.show, name="show_post"),
     path("edit/<post_id>", views.edit, name="edit_post"),
     path("delete/<int:pk>", views.delete, name="delete_post"),
+
+    path("new_video/", views.new_video, name ="new_video"),
+    path("show_video/<video_id>", views.show_video, name="show_video"),
+    path("edit_video/<video_id>", views.edit_video, name="edit_video"),
+    path("delete_video/<int:pk>", views.delete_video, name="delete_video"),
 ]   
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Post
+from .models import Video
 from .forms import PostForm
+from .forms import VideoForm
 from django.contrib import messages
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -22,10 +24,9 @@ def home(request):
     return render(request, "events/home.html", {"categories": categories})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
-        "published_date"
-    )
-    return render(request, "events/post_list.html", {"posts": posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    videos = Video.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    return render(request, "events/post_list.html", {"posts": posts, "videos": videos})
 
 def show(request, post_id):
     post = Post.objects.get(pk=post_id)
@@ -54,3 +55,31 @@ def contact(request):
     return HttpResponse(
         "This is my contact page displayed with an Http response and not a template rendering"
     )
+
+def new_video(request):
+    form = VideoForm()
+    if request.method == "POST":
+        form = VideoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Video successfully created")
+            return redirect("post_list")
+    return render(request, "events/new_video.html", {"form": form})
+
+def show_video(request, video_id):
+    video = Video.objects.get(pk=video_id)
+    return render(request, "events/show_video.html", {"video": video})
+
+def delete_video(request, pk):
+    video = Video.objects.get(pk=pk)
+    video.delete()
+    messages.success(request, "Video successfully deleted!")
+    return redirect("post_list")
+
+def edit_video(request, video_id):
+    video = Video.objects.get(pk=video_id)
+    form = VideoForm(request.POST or None, instance=video)
+    if form.is_valid():
+        form.save()
+        return redirect("post_list")
+    return render(request, "events/edit_video.html", {"video": video, "form": form})
