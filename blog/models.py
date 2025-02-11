0e466c099c58a30fbaae4342ac4eb8b22b3b5864
Nour <nour.ayed@noaxp.com>
2025-02-11 15:32:10 +0100

    Created Video model

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+        
+    def __str__(self):
+        return self.name
