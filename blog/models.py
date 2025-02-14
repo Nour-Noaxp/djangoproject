ee09a7d979941cd1c674aff2d9e43617b45086f3
Nour <nour.ayed@noaxp.com>
2025-02-14 17:58:02 +0100

    Cleaned up code

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  title = models.CharField(max_length=200,null=False)
+  title = models.CharField(max_length=200, null=False)
   text = models.TextField(null=False)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
@@ -14,8 +14,8 @@ class Post(models.Model):
     return self.title
 
 class Video(models.Model):
-  name = models.CharField(max_length=200,null=False)
-  url = models.URLField(max_length=200,null=False)
+  name = models.CharField(max_length=200, null=False)
+  url = models.URLField(max_length=200, null=False)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   published_date = models.DateTimeField(blank=True, null=True)
